7119ded6e4e9d62ffd8528426a25ab6ef570ff15
bruand81 <andbruno@unisa.it>
2019-06-28 09:30:03 +0200

    Created activity to edit and insert new emotion

---
+++ app/src/main/java/cloud/antares/diarioalimentare/EditEmotionActivity.kt
@@ -0,0 +1,12 @@
+package cloud.antares.diarioalimentare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditEmotionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_emotion)
+    }
+}
